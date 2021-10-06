8001f8790c60bcbcbde77612a00dbde6e6f920b1
LeLingu <albert.nyarko-agyei@warwick.ac.uk>
2021-10-06 14:06:00 +0100

    Standardising testing

--- files needed to run/using_model.py
+++ files needed to run/using_model.py
@@ -2,6 +2,8 @@ import time
 
 from stable_baselines3 import PPO
 import torch as th
+import numpy as np
+import matplotlib.pyplot as plt
 
 from rlohhell.envs.ohhell import OhHellEnv2
 
@@ -10,18 +12,32 @@ from rlohhell.envs.ohhell import OhHellEnv2
 env = OhHellEnv2()
 
 # Using the model
-model = PPO.load("123")
-obs = env.reset()
-total_reward = 0
-times_illegal_action = 0
-for _ in range(11):
-    action, _states = model.predict(obs)
-    print(env._decode_action(action))
-    obs, reward, dones, info = env.step(action)
-    total_reward += reward
-    print(reward)
-    if reward < 0:
-        times_illegal_action += 1
+model = PPO.load("average_score_3")
+illegals = []
+for i in range(500):
+    obs = env.reset()
+    total_reward = 0
+    times_illegal_action = 0
+    for _ in range(11):
+        available_actions = list(env._get_legal_actions())
+        # print(available_actions)
+        mask = [1 if action in available_actions else 0 for action in range(63)]
+        action, _states = model.predict(obs, mask=mask)
+        # print(action)
+        # print(env._decode_action(action))
+        obs, reward, dones, info = env.step(action)
+        total_reward += reward
+        # print(reward)
+        if reward < 0:
+            times_illegal_action += 1
 
-print('The total reward was ' + str(total_reward))
-print('The agent illegally choose ' + str(times_illegal_action))
+    illegals.append(times_illegal_action)
+    
+print("On average " + str(np.round(np.mean(illegals), 2)) + " illegal actions were selected.")
+print("The standard deviation was " + str(np.round(np.std(illegals), 2)))
+
+
+plt.hist(illegals)
+plt.show()
+
+# print('The total reward was ' + str(total_reward))
